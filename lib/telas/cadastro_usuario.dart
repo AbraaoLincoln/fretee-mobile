0daf471527994340d39bb2086e44dd66b8c3b1ae
Abraao Lincol <abraao.lincol@gmail.com>
2022-01-02 19:15:50 -0300

    #13 atualizando cadastro usuario

--- lib/telas/cadastro_usuario.dart
+++ lib/telas/cadastro_usuario.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fretee_mobile/telas/comun/fretee_api.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 
@@ -18,6 +19,10 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
   final _telefoneTextControlle = TextEditingController();
   final _nomeUsuarioTextControlle = TextEditingController();
   final _senhaTextControlle = TextEditingController();
+  String? _erroMsg;
+  String _msgFotoNaoSelecionada = "";
+  Color _borberColorImagePicker = Colors.grey.shade400;
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -39,58 +44,72 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
   }
 
   Widget _ConstruirSeletorImagem() {
-    return Container(
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey.shade400),
-          borderRadius: BorderRadius.circular(10)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: _image != null
-                  ? Image.file(
-                      _image!,
-                      width: 80,
-                      height: 80,
-                      fit: BoxFit.cover,
-                    )
-                  : const Icon(
-                      Icons.image,
-                      size: 80,
-                    ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(right: 10),
-            width: 250,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                    onPressed: selecionarImagemDaGaleria,
-                    child: const Text("Selecionar Imagem"),
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.black,
-                        primary: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: _borberColorImagePicker),
+              borderRadius: BorderRadius.circular(10)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: _image != null
+                      ? Image.file(
+                          _image!,
+                          width: 100,
+                          height: 80,
+                          fit: BoxFit.cover,
+                        )
+                      : const Icon(
+                          Icons.image,
+                          size: 80,
                         ),
-                        // elevation: 15.0,
-                        minimumSize: const Size(100, 10))),
-                const Text(
-                  "Escolha uma imagem que seu rosto esteja visivel.",
-                  style: TextStyle(fontSize: 10),
-                  textAlign: TextAlign.center,
                 ),
-              ],
-            ),
-          )
-        ],
-      ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(right: 10),
+                width: 250,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                        onPressed: selecionarImagemDaGaleria,
+                        child: const Text("Selecionar Imagem"),
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            primary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            // elevation: 15.0,
+                            minimumSize: const Size(100, 10))),
+                    const Text(
+                      "Escolha uma imagem que seu rosto esteja visivel.",
+                      style: TextStyle(fontSize: 10),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.only(top: 10),
+          margin: const EdgeInsets.only(bottom: 10),
+          child: Text(
+            _msgFotoNaoSelecionada,
+            textAlign: TextAlign.left,
+            style: TextStyle(color: _borberColorImagePicker),
+          ),
+        )
+      ],
     );
   }
 
@@ -101,6 +120,8 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
       final imageFile = File(imagem.path);
       setState(() {
         this._image = imageFile;
+        _msgFotoNaoSelecionada = "";
+        _borberColorImagePicker = Colors.grey.shade400;
       });
     } on PlatformException catch (e) {
       print("Não foi possivel selecionar a imagem");
@@ -109,35 +130,38 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
 
   Widget _ConstruirformularioDeCadastro() {
     return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          _ConstruirInputDoFormulatio("Nome Completo",
-              _nomeCompletoTextControlle, TextInputType.text, false),
-          _ConstruirInputDoFormulatio(
-              "Telefone", _telefoneTextControlle, TextInputType.number, false),
-          _ConstruirInputDoFormulatio("Nome de Usuario",
-              _nomeUsuarioTextControlle, TextInputType.text, false),
-          _ConstruirInputDoFormulatio(
-              "Senha", _senhaTextControlle, TextInputType.text, true),
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            child: TextButton(
-                onPressed: _cadastrarUsuario,
-                child: Text(
-                  "Cadastrar",
-                  style: TextStyle(fontSize: 20),
-                ),
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    primary: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    // elevation: 15.0,
-                    minimumSize: Size(100, 50))),
-          )
-        ],
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _ConstruirInputDoFormulatio("Nome Completo",
+                _nomeCompletoTextControlle, TextInputType.text, false, false),
+            _ConstruirInputDoFormulatio("Telefone", _telefoneTextControlle,
+                TextInputType.number, false, false),
+            _ConstruirInputDoFormulatio("Nome de Usuario",
+                _nomeUsuarioTextControlle, TextInputType.text, false, true),
+            _ConstruirInputDoFormulatio(
+                "Senha", _senhaTextControlle, TextInputType.text, true, false),
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              child: TextButton(
+                  onPressed: _cadastrarUsuario,
+                  child: const Text(
+                    "Cadastrar",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      primary: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      // elevation: 15.0,
+                      minimumSize: const Size(100, 50))),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -146,22 +170,26 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
       String label,
       TextEditingController controller,
       TextInputType keyboardType,
-      bool obscureText) {
+      bool obscureText,
+      bool userName) {
     return Container(
-      margin: EdgeInsets.only(top: 20),
+      margin: const EdgeInsets.only(bottom: 20),
       child: TextFormField(
           keyboardType: keyboardType,
           decoration: InputDecoration(
               labelText: label,
-              labelStyle:
-                  TextStyle(color: Colors.black45, fontWeight: FontWeight.bold),
+              labelStyle: const TextStyle(
+                  color: Colors.black45, fontWeight: FontWeight.bold),
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(10),
                 borderSide: BorderSide(color: Colors.grey.shade400),
-              )),
+              ),
+              errorText: userName ? _erroMsg : null),
           controller: controller,
           validator: (value) {
-            if (value!.isEmpty) return "Insira o/a ${label}";
+            if (value!.isEmpty) {
+              return "Insira o/a ${label}";
+            }
           },
           obscureText: obscureText,
           enableSuggestions: !obscureText,
@@ -170,8 +198,10 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
   }
 
   void _cadastrarUsuario() async {
-    var request = http.MultipartRequest(
-        "POST", Uri.parse("http://192.168.0.243:8080/fretee/api/usuario/"));
+    if (!validateImageAndForm()) return;
+
+    var request =
+        http.MultipartRequest("POST", FreteeApi.getUriCadastrarUsuario());
 
     request.fields["nome"] = _nomeCompletoTextControlle.text;
     request.fields["telefone"] = _telefoneTextControlle.text;
@@ -183,5 +213,29 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
     request.files.add(imageUsuario);
 
     var response = await request.send();
+
+    switch (response.statusCode) {
+      case 400:
+        var error = response.headers["error"];
+        setState(() {
+          _erroMsg = error;
+        });
+        break;
+    }
+  }
+
+  bool validateImageAndForm() {
+    if (_image == null) {
+      setState(() {
+        _msgFotoNaoSelecionada = "Selecione uma foto";
+      });
+      _borberColorImagePicker = Colors.red;
+      return false;
+    }
+
+    var form = _formKey.currentState;
+    if (!form!.validate()) return false;
+
+    return true;
   }
 }
---
+++ lib/telas/comun/fretee_api.dart
@@ -0,0 +1,13 @@
+class FreteeApi {
+  static String url = "http://192.168.0.243:8080/api";
+  static String loginUrn = "/autenticacao/login";
+  static String cadastroUsuarioUrn = "/usuario";
+
+  static Uri getLoginUri() {
+    return Uri.parse(url + loginUrn);
+  }
+
+  static Uri getUriCadastrarUsuario() {
+    return Uri.parse(url + cadastroUsuarioUrn);
+  }
+}
